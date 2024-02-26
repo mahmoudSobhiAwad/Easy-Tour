1d90b191766f62d80c4a818cdaacc34f673c5d9b
GAMING STORE <modyelgen243@gmail.com>
2024-02-27 00:05:11 +0200

    edit_app_bar_shape

--- lib/features/tourist/tourist_home/presentaion/view/widgets/app_bar.dart
+++ lib/features/tourist/tourist_home/presentaion/view/widgets/app_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:prepare_project/core/utilities/textStyle/font_styles.dart';
-import 'package:prepare_project/core/widget/tour_guide/custom_border_raduis.dart';
 import 'package:prepare_project/features/tourist/toursit_search/presentation/view/search_view.dart';
 import '../../../../../../core/utilities/basics.dart';
 
@@ -28,27 +27,22 @@ class HomeTouristTourGuideAppBar extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            IconButton(onPressed: openSidePar, icon: const Icon(Icons.table_rows_rounded,color: basicColor,),padding: EdgeInsets.zero,),
+            IconButton(onPressed: openSidePar, icon: const Icon(Icons.table_rows_rounded,color: basicColor,),),
+            SizedBox(width: width*0.5,child: const Align(alignment: Alignment.centerLeft,child: Text('EGY Tour',style: CustomTextStyle.homePartTitle,))),
             GestureDetector(
               onTap: (){
                 Navigator.push(context, MaterialPageRoute(builder:(context)=>const SearchView()));
               },
-              child: SizedBox(
+              child: Padding(
+                padding: const EdgeInsets.only(top:10.0),
                 child: Container(
-                  width:width*0.65,
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: commonBorderRadius(),
+                  padding: const EdgeInsets.only(bottom: 15,top: 5,left: 10,right: 10),
+                  decoration: const BoxDecoration(
                     color: thirdColor,
+                    shape: BoxShape.circle,
                   ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Search...',style:CustomTextStyle.commonFontThinLight),
-                      Icon( Icons.search_rounded),
-                    ],
-                  ),
-                ),),
+                    child: const Icon( Icons.search_rounded,size: 33,)),
+              ),
             ),
             rightIcon,
           ],
