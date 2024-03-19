4b58a78d7f1d69033b38e5040af38a961d4ab676
GAMING STORE <modyelgen243@gmail.com>
2024-03-20 00:08:12 +0200

    edit ui for send request to generate trip with ai

---
+++ lib/features/tourist/generate_trip_with_ai/presentation/views/widgets/add_new_destination.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:prepare_project/core/utilities/basics.dart';
+import 'package:prepare_project/core/utilities/textStyle/font_styles.dart';
+class AddNewDestination extends StatelessWidget {
+  const AddNewDestination({
+    super.key,
+    required this.addNewDestination,
+    required this.width,
+  });
+
+  final void Function() addNewDestination;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: addNewDestination,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5.0,top: 5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const SizedBox(
+                height: 10,
+                child: VerticalDivider(thickness: 2,color: secondaryColor,)),
+            Container(
+              width: width*0.45,
+              padding:const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: const Color(0xffF57366).withOpacity(0.74),
+                borderRadius: BorderRadius.circular(7),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.add,color: Colors.white,),
+                  Text('Add New Destination',style: CustomTextStyle.tripDetailsInTourGuide.copyWith(color: Colors.white),)
+                ],
+              ),
+            )
+          ],),
+      ),
+    );
+  }
+}
---
+++ lib/features/tourist/generate_trip_with_ai/presentation/views/widgets/place_range_time_list.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:prepare_project/core/utilities/basics.dart';
+import 'package:prepare_project/core/utilities/textStyle/font_styles.dart';
+import 'package:prepare_project/core/widget/sign_up_edit/custom_drop_down.dart';
+import 'package:prepare_project/core/widget/tour_guide/custom_border_raduis.dart';
+import 'package:prepare_project/features/tourist/generate_trip_with_ai/data/data_ui.dart';
+import 'package:prepare_project/features/tourist/generate_trip_with_ai/data/model/destination_with_number_model.dart';
+class PlaceWithRangeTimeList extends StatelessWidget {
+  const PlaceWithRangeTimeList({
+    super.key,
+    required this.height,
+    required this.destinationList,
+    required this.width,
+    required this.onSelect,
+    required this.pickDate,
+    required this.removeAt,
+  });
+
+  final double height;
+  final List<DestinationPlaceDayModel> destinationList;
+  final double width;
+  final void Function(String? value, int index) onSelect;
+  final void Function(int index) pickDate;
+  final void Function(int index) removeAt;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height*0.07*destinationList.length+(destinationList.length>1?8:0),
+      child: ListView.separated(
+        clipBehavior: Clip.antiAlias,
+        padding: EdgeInsets.zero,
+        itemCount: destinationList.length,
+        itemBuilder:(context,index){
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomDropDownMenu(
+                width: width*0.35,
+                fontSize: 12,
+                fillColor: forthColor,
+                color: Colors.white,
+                list: placesForGeneratedTrip,
+                label: 'Pick Place',
+                enableSearch: true,
+                labelStyle: CustomTextStyle.commonFontThinLight.copyWith(color: Colors.white),
+                onSelected:(String?value){
+                  onSelect(value,index);
+                },
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                height: height*0.07,
+                width: width*0.48,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: commonBorderRadius(),
+                  boxShadow: [buildBoxShadow()],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    destinationList[index].numOfDay==null? const Text('Pick Range Of Date',style: CustomTextStyle.tripDetailsInTourGuide):Text('From: ${destinationList[index].startDate}\nTo : ${destinationList[index].lastDate}',style: CustomTextStyle.tripDetailsInTourGuide,),
+                    IconButton(onPressed:(){
+                      pickDate(index);
+                    },icon:const Icon(Icons.calendar_month_rounded,color: basicColor,)),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                  onTap:(){removeAt(index);},
+                  child: const Icon(Icons.delete,color: closeColor,)),
+            ],
+          );
+        },
+        separatorBuilder:(context,index){
+          return const SizedBox(height: 8,);
+        } ,
+      ),
+    );
+  }
+}
